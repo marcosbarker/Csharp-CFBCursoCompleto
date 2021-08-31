ba5b4917470854aa5ddebba63950a06b2c50d7fd
marcosbarker <marcos_barker@hotmail.com>
2021-08-31 00:25:42 -0300

    code: aula07

---
+++ aula07/Aula07.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Aula07 {
+  static void Main() {
+    const string canal = "CFB cursos";
+    const double pi = 3.1415;
+
+    Console.WriteLine("Canal {0}  pi: {1}", canal, pi);
+  }
+}
