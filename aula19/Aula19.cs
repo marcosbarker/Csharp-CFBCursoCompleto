721c31d5b002956c06c6d9a38f6010be793a9bc4
marcosbarker <marcos_barker@hotmail.com>
2021-08-31 16:04:04 -0300

    code: aula19

---
+++ aula19/Aula19.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Aula19 {
+  static void Main() {
+    //loop for (para) / estruturas de iteracao
+    //for (inicializador; definicao da repeticao; incrementoOuDecremento)
+    //for (int num=0; num < 1; num++)
+    //for (e1; e2; e3) 
+    //e1: executa apenas na primeira vez
+    //e2: testa, executa os comandos
+    //e3: incrementa, volta para e2
+
+    for (int num=0; num < 10; num++) {
+      Console.WriteLine("Teste de repetição, valor: " + num);
+    }
+  }
+}
---
+++ aula19/Aula191.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aula19 {
+  static void Main() {
+
+    int[] num = new int[10]; 
+
+    for (int i=0; i < num.Length; i++) {
+      num[i] = 0;
+
+      Console.WriteLine("Teste de repetição, valor: " + i);
+    }
+  }
+}
