84d1973651322f0773274b5a336b7fd6ab52d9a1
marcosbarker <marcos_barker@hotmail.com>
2021-09-22 16:30:09 -0300

    code: aula40

---
+++ aulas/aula40/Aula40.cs
@@ -0,0 +1,13 @@
+using System;
+//classe sealed (selada)
+//não pode ser herdada
+//não atende aos conceitos de herança
+
+sealed class Veiculo {
+//declaração comum
+}
+class Aula40 {
+  static void Main() {
+
+  }
+}
